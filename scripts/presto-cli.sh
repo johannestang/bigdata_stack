80d881c507ad60e563be604cf6caa025d0df0407
Johannes Tang Kristensen <johs@johs.me>
2019-12-10 15:16:15 +0000

    Updated Presto to 326 and Minio to latest.

--- scripts/presto-cli.sh
+++ scripts/presto-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "${0%/*}"
 
-PRESTO_VERSION=0.215
+PRESTO_VERSION=326
 if [ ! -e presto-cli.jar ]; then
-    wget https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/$PRESTO_VERSION/presto-cli-$PRESTO_VERSION-executable.jar -O presto-cli.jar
+    wget https://repo1.maven.org/maven2/io/prestosql/presto-cli/$PRESTO_VERSION/presto-cli-$PRESTO_VERSION-executable.jar -O presto-cli.jar
 fi 
 java -jar presto-cli.jar --server localhost:8080 --catalog hive --schema default
